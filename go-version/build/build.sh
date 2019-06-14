626527ce4ee6a13d1b96ce8ee65b345626f60237
Joe Reynolds <joe.reynolds@sykescottages.co.uk>
2019-06-14 15:57:24 +0100

    Work on build script

--- go-version/build/build.sh
+++ go-version/build/build.sh
@@ -1,2 +1,8 @@
+gofmt -w ./main.go
 shelltest test.txt
-gofmt main.go
+
+# Versions are bound to a commit id, each commit id gets a reference in the dist directory
+commit=$(git rev-parse --short HEAD)
+output_dir="./dist/$commit"
+mkdir -p "$output_dir"
+go build -o "$output_dir/tf" ./main.go
--- go-version/main.go
+++ go-version/main.go
@@ -1,35 +1,35 @@
 package main
 
 import (
-    "fmt"   
-    "bufio"
-    "os"
-    "strings"
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
 )
 
 func main() {
-    reader := bufio.NewReader(os.Stdin)
-    text, _:= reader.ReadString('\n')
+	reader := bufio.NewReader(os.Stdin)
+	text, _ := reader.ReadString('\n')
 
-    if len(os.Args) < 2 {
-        fmt.Println("Expected one of the following subcommmands: 'upper', 'lower', 'title', 'join'")
-        os.Exit(1)
-    }
+	if len(os.Args) < 2 {
+		fmt.Println("Expected one of the following subcommmands: 'upper', 'lower', 'title', 'join'")
+		os.Exit(1)
+	}
 
-    switch os.Args[1] {
+	switch os.Args[1] {
 
-      case "upper":
-        fmt.Println(strings.ToUpper(text))
-    case "lower":
-        fmt.Println(strings.ToLower(text))
-    case "title":
-        fmt.Println(strings.Title(text))
-    case "join":
-        fmt.Println(
-            strings.Join(
-                strings.Split(text, " "),
-                os.Args[2],
-            ),
-        )
-    }
+	case "upper":
+		fmt.Println(strings.ToUpper(text))
+	case "lower":
+		fmt.Println(strings.ToLower(text))
+	case "title":
+		fmt.Println(strings.Title(text))
+	case "join":
+		fmt.Println(
+			strings.Join(
+				strings.Split(text, " "),
+				os.Args[2],
+			),
+		)
+	}
 }
